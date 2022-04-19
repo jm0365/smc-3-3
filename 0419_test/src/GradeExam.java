a5c148dbb3115778d00eef0f66ea9fe2d1454751
jmlee <jm0365@sen.go.kr>
2022-04-19 15:03:01 +0900

    0419 과제

---
+++ 0419_test/src/GradeExam.java
@@ -0,0 +1,16 @@
+
+public class GradeExam {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class Grade{
+	int math;
+	int science;
+	int english;
+	public int average() {
+		return (math+science+english)/3;
+	}
+}
