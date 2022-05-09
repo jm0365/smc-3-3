7d91b31f5f549c69e0b0b98f321a330239875e80
JMLEE <jm0365@sen.go.kr>
2022-05-10 08:37:37 +0900

    0510 과제

---
+++ 0510/src/Number.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Number {
+	public static void main(String[] args) {
+        System.out.print("10개의 랜덤 숫자 중 하나를 맞춰보세요(1~50까지): ");
+        Scanner sc=new Scanner(System.in);
+        int num = sc.nextInt();
+        int []arr=new int[10];
+ 
+	}
+}
+
+
