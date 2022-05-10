37a26efcc6542ce3a6c34b99087d1586465510d9
JMLEE <jm0365@sen.go.kr>
2022-05-10 10:18:57 +0900

    0510 프로젝트 추가

--- 0510/src/Sum.java
+++ 0510/src/Sum.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 public class Sum {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+		int a,b=0;  // 입력 받는 변수
+		int sum=0;  // 합계 저장
+		System.out.print("시작 숫자: ");
+		a = scan.nextInt();
+		System.out.print("마지막 숫자: ");
+		b = scan.nextInt();
+		for(int i=a; i<=b; i++) {
+			sum += i;   // sum = sum+i;
+		}
+		System.out.println(a+" 부터"+b+" 까지의 합은 "+sum);
 	}
 }
+
+
+
